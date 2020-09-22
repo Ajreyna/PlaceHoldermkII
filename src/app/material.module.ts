2fc32d1559959d279192146533eb8031eab372c2
ajreyna <ajreyna88@gmail.com>
2020-09-22 12:07:44 -0500

    11

---
+++ src/app/material.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
+import { MatSortModule } from '@angular/material/sort';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggle, MatButtonToggleModule } from '@angular/material/button-toggle';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    MatTableModule,
+    MatSortModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+  ],
+
+  exports: [
+    MatTableModule,
+    MatSortModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+  ]
+})
+export class MaterialModule { }
